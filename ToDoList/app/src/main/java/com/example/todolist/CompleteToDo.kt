44ea0231bb86d4840d5b99091dffdf5ce804bd85
michael.zech <michael.zech@hotmail.de>
2020-05-17 08:48:17 +0200

    'ToDo'-App: Finished 'Complete ToDo'-activity.

--- ToDoList/app/src/main/java/com/example/todolist/CompleteToDo.kt
+++ ToDoList/app/src/main/java/com/example/todolist/CompleteToDo.kt
@@ -1,5 +1,7 @@
 package com.example.todolist
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,13 +9,25 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_complete_to_do.*
 
 class CompleteToDo : AppCompatActivity() {
+    var sToDoText: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complete_to_do)
 
-        var sToDoText = intent.getStringExtra("toDoText")
+        sToDoText = intent.getStringExtra("toDoText")
 
         toDoText.text = sToDoText
-        Log.i("###", sToDoText)
+
+        complete.setOnClickListener {
+            var prefs = getSharedPreferences(getString(R.string.SHARED_PREF_NAME)
+                , Context.MODE_PRIVATE)
+            var todos = prefs.getStringSet(getString(R.string.TODO_STRINGS)
+                , mutableSetOf())
+            var editor = prefs.edit()
+            todos!!.remove(sToDoText)
+            editor.putStringSet(getString(R.string.TODO_STRINGS), todos)
+            editor.apply()
+            finish()
+        }
     }
 }
