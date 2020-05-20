7af79c138c40bd0d28ddf89c4fddcfe97bbfb717
michael.zech <michael.zech@hotmail.de>
2020-05-20 08:40:41 +0200

    'ToDo-List': Added helper-function setRecyclerView().

--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todolist
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -13,24 +14,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     lateinit var layoutManager: LinearLayoutManager
     lateinit var adapter: ItemsAdapter
+    var prefs: SharedPreferences? = null
+    var set: MutableSet<String>? = null
+    var editor: SharedPreferences.Editor? = null
+
+    fun setRecyclerView() {
+        set = prefs?.getStringSet(getString(R.string.TODO_STRINGS), mutableSetOf())
+
+        layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager
+        adapter = ItemsAdapter(set!!, this)
+        recycler_view.adapter = adapter
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        var prefs
-                = getSharedPreferences(getString(R.string.SHARED_PREF_NAME), Context.MODE_PRIVATE)
-        var set
-                = prefs.getStringSet(getString(R.string.TODO_STRINGS), mutableSetOf())
-
-        val editor = prefs.edit()
-        editor.clear().commit()
-
-        layoutManager = LinearLayoutManager(this)
-        recycler_view.layoutManager = layoutManager
-        adapter = ItemsAdapter(set!!, this)
-        recycler_view.adapter = adapter
+        prefs = getSharedPreferences(getString(R.string.SHARED_PREF_NAME), Context.MODE_PRIVATE)
+        editor = prefs?.edit()
+        editor?.clear()?.commit()
+        setRecyclerView()
 
         fab.setOnClickListener { view ->
             val intent = Intent(applicationContext, CreateToDo::class.java)
@@ -41,38 +46,18 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        var prefs
-                = getSharedPreferences(getString(R.string.SHARED_PREF_NAME), Context.MODE_PRIVATE)
-        val set
-                = prefs.getStringSet(getString(R.string.TODO_STRINGS), mutableSetOf())
-
-        layoutManager = LinearLayoutManager(this)
-        recycler_view.layoutManager = layoutManager
-        adapter = ItemsAdapter(set!!, this)
-        recycler_view.adapter = ItemsAdapter(set!!, this)
+        setRecyclerView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_delete) {
-            var prefs
-                    = getSharedPreferences(getString(R.string.SHARED_PREF_NAME), Context.MODE_PRIVATE)
-            var set
-                    = prefs.getStringSet(getString(R.string.TODO_STRINGS), mutableSetOf())
-
-            val editor = prefs.edit()
-            editor.clear().commit()
-
-            layoutManager = LinearLayoutManager(this)
-            recycler_view.layoutManager = layoutManager
-            adapter = ItemsAdapter(set!!, this)
-            recycler_view.adapter = ItemsAdapter(set!!, this)
-
+            editor?.clear()?.commit()
+            setRecyclerView()
             recreate()
 
             return true
