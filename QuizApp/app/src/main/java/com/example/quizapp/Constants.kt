a02409b9ce175a8d64a3b8fd89ee021515caa19f
michael.zech <michael.zech@hotmail.de>
2020-06-25 08:03:08 +0200

    Added the layout for the question-activity.

--- QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
@@ -6,19 +6,19 @@ object Constants {
 
         val que1 = Question(1,
             "What country does these flag belong to?",
-            "", "Japan", "USA", "China",
-            "France", 2)
+            "France", "Japan", "USA", "China",
+            "France", 4)
         val que2 = Question(2,
             "What country does these flag belong to?",
-            "", "Germany", "Italy", "UK",
-            "Netherlands", 1)
+            "Netherlands", "Germany", "Italy", "UK",
+            "Netherlands", 4)
         val que3 = Question(3,
             "What country does these flag belong to?",
-            "", "Belgium", "Russia", "India",
-            "France", 3)
+            "Russia", "Belgium", "Russia", "India",
+            "France", 2)
         val que4 = Question(4,
             "What country does these flag belong to?",
-            "", "Brazil", "Portugal", "Spain",
+            "Brazil", "Brazil", "Portugal", "Spain",
             "Argentina", 1)
 
         questionsList.add(que1)
--- QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,6 +2,8 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
 
@@ -10,6 +12,17 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_questions)
 
         val questionsList = Constants.getQuestions()
-        
+        Log.i("###", "${questionsList.size.toString()}")
+
+        var index = 0
+        var firstQuestion = questionsList.get(index)
+
+        progressbar.progress = index
+        tv_question.setText(firstQuestion.question)
+        tv_image.setText(firstQuestion.image)
+        tv_option_one.setText(firstQuestion.optionOne)
+        tv_option_two.setText(firstQuestion.optionTwo)
+        tv_option_three.setText(firstQuestion.optionThree)
+        tv_option_four.setText(firstQuestion.optionFour)
     }
 }
