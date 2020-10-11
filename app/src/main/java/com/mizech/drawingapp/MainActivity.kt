eeb78449eac28b79d1f6ab598d699ea5185b3fc2
michael.zech <michael.zech@hotmail.de>
2020-10-11 12:41:53 +0200

    Menu "Select Brush Size".

--- app/src/main/java/com/mizech/drawingapp/MainActivity.kt
+++ app/src/main/java/com/mizech/drawingapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mizech.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         drawing_view.setSizeForBrush(20.toFloat())
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Choose a brush size!")
+        val smallButton = brushDialog.ib_small_brush
+        val mediumButton = brushDialog.ib_medium_brush
+        val largeButton = brushDialog.ib_large_brush
+
+        smallButton.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumButton.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        largeButton.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
