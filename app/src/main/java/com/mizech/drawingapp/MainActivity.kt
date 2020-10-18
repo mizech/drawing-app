faf9f099340a8d9abe02ff8f6a157e2572a9a212
michael.zech <michael.zech@hotmail.de>
2020-10-18 15:48:22 +0200

    Selecting different colors.

--- app/src/main/java/com/mizech/drawingapp/MainActivity.kt
+++ app/src/main/java/com/mizech/drawingapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mizech.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
@@ -51,5 +52,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         brushDialog.show()
+    }
+
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
+            val imageButton = view as ImageButton
+
+            val colorTag = imageButton.tag.toString()
+            drawing_view.setColor(colorTag)
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+            mImageButtonCurrentPaint = view
+        }
     }
 }
